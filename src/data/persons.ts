4822e0bdd50a873dcc9c45662f7ce89e0f3695fb
IvanKazakovWork <IvanKazakovWork@yandex.ru>
2025-11-21 20:52:19 +0300

    Update persons.ts

--- src/data/persons.ts
+++ src/data/persons.ts
@@ -1,5 +1,19 @@
 import { Person } from "@/types/quiz";
 import answer1 from "@/assets/answer-1.jpg";
+import answer2 from "@/assets/answer-2.jpg";
+import answer3 from "@/assets/answer-3.jpg";
+import answer4 from "@/assets/answer-4.jpg";
+import answer5 from "@/assets/answer-5.jpg";
+import answer6 from "@/assets/answer-6.jpg";
+import answer7 from "@/assets/answer-7.jpg";
+import answer8 from "@/assets/answer-8.jpg";
+import answer9 from "@/assets/answer-9.jpg";
+import answer10 from "@/assets/answer-10.jpg";
+import answer11 from "@/assets/answer-11.jpg";
+import answer12 from "@/assets/answer-12.jpg";
+import answer13 from "@/assets/answer-13.jpg";
+import answer14 from "@/assets/answer-14.jpg";
+import answer15 from "@/assets/answer-15.jpg";
 
 // Placeholder image - замените на реальные изображения людей
 const placeholderImage = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='200' height='200'%3E%3Crect width='200' height='200' fill='%23000000'/%3E%3C/svg%3E";
@@ -8,18 +22,18 @@ const placeholderImage = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/200
 
 export const persons: Person[] = [
   { id: 1, name: "Даша Симонова", image: answer1 },
-  { id: 2, name: "Вика", image: placeholderImage },
-  { id: 3, name: "Олеся", image: placeholderImage },
-  { id: 4, name: "Лиза", image: placeholderImage },
-  { id: 5, name: "Матвей", image: placeholderImage },
-  { id: 6, name: "Илья", image: placeholderImage },
-  { id: 7, name: "Даша Симка", image: placeholderImage },
-  { id: 8, name: "Тася", image: placeholderImage },
-  { id: 9, name: "Арина", image: placeholderImage },
-  { id: 10, name: "Егор", image: placeholderImage },
-  { id: 11, name: "Даша Пономарева", image: placeholderImage },
-  { id: 12, name: "Таня", image: placeholderImage },
-  { id: 13, name: "Амир", image: placeholderImage },
-  { id: 14, name: "Вадим", image: placeholderImage },
-  { id: 15, name: "Егор Леонов", image: placeholderImage },
+  { id: 2, name: "Вика", image: answer2 },
+  { id: 3, name: "Олеся", image: answer3 },
+  { id: 4, name: "Лиза", image: answer4 },
+  { id: 5, name: "Матвей", image: answer5 },
+  { id: 6, name: "Илья", image: answer6 },
+  { id: 7, name: "Даша Симка", image: answer7 },
+  { id: 8, name: "Тася", image: answer8 },
+  { id: 9, name: "Арина", image: answer9 },
+  { id: 10, name: "Егор", image: answer10 },
+  { id: 11, name: "Даша Пономарева", image: answer11 },
+  { id: 12, name: "Таня", image: answer12 },
+  { id: 13, name: "Амир", image: answer13 },
+  { id: 14, name: "Вадим", image: answer14 },
+  { id: 15, name: "Егор Леонов", image: answer15 },
 ];
